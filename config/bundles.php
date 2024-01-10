7791e39d6175c476e87f9683d6109aced84fcb40
Дмитрий Анохин <kanokhin@Noutbuk-Dmitrij.local>
2024-01-10 17:00:48 +0300

    10.01.12

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
